6d2fae95c52585161f5fc111ebd1ecdea4288ec2
Henrique Teixeira <henriqueteixeira26@gmail.com>
2023-07-13 09:52:31 -0300

    Test Coontdown LocalStorege

--- app/components/WalletConnection/MyCountdown.jsx
+++ app/components/WalletConnection/MyCountdown.jsx
@@ -3,7 +3,7 @@
 
 // const Completionist = () => <span>Contagem regressiva finalizada!</span>;
 
-// const MyCountdown = ({ days, hours, minutes, seconds,}) => (
+// const MyCountdown = ({ days, hours, minutes, seconds }) => (
 //   <div className="group-one flex flex-row space-x-4">
 //     <div className="basis-1/4 flex flex-col justify-center text-center  bg-[#40A072] rounded-md h-10 border border-gray-500 hover:border-green-300">
 //       <p>{days}</p>
@@ -25,14 +25,31 @@
 // );
 
 // export default function MyPage() {
+//   const renderer = ({ days, hours, minutes, seconds, completed }) => {
+//     if (completed) {
+//       // Render a complete state
+//       return <Completionist />;
+//     } else {
+//       // Render a countdown
+//       return (
+//         <MyCountdown
+//           days={days}
+//           hours={hours}
+//           minutes={minutes}
+//           seconds={seconds}
+//         />
+//       );
+//     }
+//   };
+
 //   return (
 //     <>
-//       <MyCountdown />
+//       <Countdown date={Date.now() + 1296000000} renderer={renderer} />
 //     </>
-//   )
+//   );
 // }
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Countdown from 'react-countdown';
 
 const Completionist = () => <span>Contagem regressiva finalizada!</span>;
@@ -59,6 +76,19 @@ const MyCountdown = ({ days, hours, minutes, seconds }) => (
 );
 
 export default function MyPage() {
+  const [timeLeft, setTimeLeft] = useState(Date.now() + 1296000000);
+
+  useEffect(() => {
+    const savedTime = localStorage.getItem('timeLeft');
+    if (savedTime) {
+      setTimeLeft(savedTime);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('timeLeft', timeLeft);
+  }, [timeLeft]);
+
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
       // Render a complete state
@@ -78,7 +108,7 @@ export default function MyPage() {
 
   return (
     <>
-      <Countdown date={Date.now() + 1296000000} renderer={renderer} />
+      <Countdown date={timeLeft} renderer={renderer} />
     </>
   );
 }
